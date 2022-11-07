98dff911041413ec99589e6d9ddb75d53765cdeb
nazmulrony <nhrony65@gmail.com>
2022-11-07 21:47:07 +0600

    first commit

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyC9xcn-ufUlw4d4NH9C-i0or4NOFqYmPMk",
+    authDomain: "creative-photography-nhrony.firebaseapp.com",
+    projectId: "creative-photography-nhrony",
+    storageBucket: "creative-photography-nhrony.appspot.com",
+    messagingSenderId: "369854754849",
+    appId: "1:369854754849:web:7c59331edc17330b0c7360"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
